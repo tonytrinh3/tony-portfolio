6d34f7fae53a197d67d9f1e762d8e73ba4043e0d
tony trinh <tony.w.trinh3@gmail.com>
2020-06-25 21:25:56 -0700

    Updates

---
+++ precache-manifest.921eab530d0ca27e59d95e56a2fd7473.js
@@ -0,0 +1,174 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89fed11dc62d53d6e83bb44a6fc5f4f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ea2f55d0dcbf83449b40",
+    "url": "/static/css/2.1c164e0d.chunk.css"
+  },
+  {
+    "revision": "c58dd4cbd6300f52621b",
+    "url": "/static/css/main.fa1f522a.chunk.css"
+  },
+  {
+    "revision": "ea2f55d0dcbf83449b40",
+    "url": "/static/js/2.0ab95c0c.chunk.js"
+  },
+  {
+    "revision": "4648310631ffed799e36605e1d4f0bae",
+    "url": "/static/js/2.0ab95c0c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c58dd4cbd6300f52621b",
+    "url": "/static/js/main.cfab20c7.chunk.js"
+  },
+  {
+    "revision": "4fab6b39897d26240400",
+    "url": "/static/js/runtime-main.8ccaace2.js"
+  },
+  {
+    "revision": "e0f651ac7aecf9187f66e5bd5f181c10",
+    "url": "/static/media/Sass_Logo_Color.e0f651ac.svg"
+  },
+  {
+    "revision": "03783c5172ee1ad128c576bf88fac168",
+    "url": "/static/media/fa-brands-400.03783c51.eot"
+  },
+  {
+    "revision": "073c2f3ce60eaf69cc2767ef3d989078",
+    "url": "/static/media/fa-brands-400.073c2f3c.svg"
+  },
+  {
+    "revision": "7559b3774a0625e8ca6c0160f8f6cfd8",
+    "url": "/static/media/fa-brands-400.7559b377.woff2"
+  },
+  {
+    "revision": "ed2b8bf117160466ba6220a8f1da54a4",
+    "url": "/static/media/fa-brands-400.ed2b8bf1.ttf"
+  },
+  {
+    "revision": "fe9d62e0d16a333a20e63c3e7595f82e",
+    "url": "/static/media/fa-brands-400.fe9d62e0.woff"
+  },
+  {
+    "revision": "4bced7c4c0d61d4f988629bb8ae80b8b",
+    "url": "/static/media/fa-solid-900.4bced7c4.woff"
+  },
+  {
+    "revision": "acf50f59802f20d8b45220eaae532a1c",
+    "url": "/static/media/fa-solid-900.acf50f59.ttf"
+  },
+  {
+    "revision": "b557f56e367e59344ca95f9d1fb44352",
+    "url": "/static/media/fa-solid-900.b557f56e.svg"
+  },
+  {
+    "revision": "b5cf8ae26748570d8fb95a47f46b69e1",
+    "url": "/static/media/fa-solid-900.b5cf8ae2.woff2"
+  },
+  {
+    "revision": "ef3df98419d143d9617fe163bf4edc0b",
+    "url": "/static/media/fa-solid-900.ef3df984.eot"
+  },
+  {
+    "revision": "e423e6d4c8790557c5968dfaa76cebb5",
+    "url": "/static/media/github-image.e423e6d4.svg"
+  },
+  {
+    "revision": "99040ef892bb0ab85b7c4c7d2c5578be",
+    "url": "/static/media/headshot.99040ef8.jpg"
+  },
+  {
+    "revision": "ce69286c9df18c104e45b4663c835369",
+    "url": "/static/media/html-css-js.ce69286c.png"
+  },
+  {
+    "revision": "2c3c135cc8e06e14eb916df38e89b3fb",
+    "url": "/static/media/jest-logo.2c3c135c.svg"
+  },
+  {
+    "revision": "007eb41ac7dadcaf97d565a787bf8afc",
+    "url": "/static/media/linkedin.007eb41a.svg"
+  },
+  {
+    "revision": "e6956db22f6b55500046d41bb128484a",
+    "url": "/static/media/mail.e6956db2.svg"
+  },
+  {
+    "revision": "d9293289cc43ede403b002d2b2c1bce0",
+    "url": "/static/media/mock-airbnb-11.d9293289.PNG"
+  },
+  {
+    "revision": "cf11143e5f94923d95fb4befa02ae5ec",
+    "url": "/static/media/mock-airbnb-22.cf11143e.PNG"
+  },
+  {
+    "revision": "aede34b4704681f3ec3bf0835a5270e8",
+    "url": "/static/media/mock-airbnb-33.aede34b4.PNG"
+  },
+  {
+    "revision": "b126cc43202155120d85c97539c22460",
+    "url": "/static/media/mock-airbnb-4.b126cc43.PNG"
+  },
+  {
+    "revision": "ceede623d23da46d58bdf830a042f916",
+    "url": "/static/media/mock-airbnb-data.ceede623.png"
+  },
+  {
+    "revision": "7f86207f9cbbb45bf0594abc6c1b90bd",
+    "url": "/static/media/mock-airbnb-db.7f86207f.png"
+  },
+  {
+    "revision": "9861fe8b1a8eff4b374dca0aae69c781",
+    "url": "/static/media/mock-airbnb-front-2.9861fe8b.PNG"
+  },
+  {
+    "revision": "66402507c61356369375df78e8711203",
+    "url": "/static/media/mock-airbnb-front-page.66402507.PNG"
+  },
+  {
+    "revision": "1304f8b4619271b737e240feb49066c8",
+    "url": "/static/media/mock-airbnb-login.1304f8b4.png"
+  },
+  {
+    "revision": "437721f50d0e9cbcccd140b185efacf3",
+    "url": "/static/media/mock-airbnb-react-redux.437721f5.png"
+  },
+  {
+    "revision": "601de98937e69437f62a98bca826babd",
+    "url": "/static/media/mock-airbnb-route.601de989.png"
+  },
+  {
+    "revision": "b30ceef7f7e81c7dc160af90229e9775",
+    "url": "/static/media/mongodb.b30ceef7.svg"
+  },
+  {
+    "revision": "72345933059c42c02fba22df4cdcb99d",
+    "url": "/static/media/mountain.72345933.jpg"
+  },
+  {
+    "revision": "be2df79601b32143d336bd076429b909",
+    "url": "/static/media/nodejs.be2df796.svg"
+  },
+  {
+    "revision": "30ade27f3620e47c991768c839051005",
+    "url": "/static/media/portfolio.30ade27f.PNG"
+  },
+  {
+    "revision": "934eab62e954a6d5ae9a39d09dd192ff",
+    "url": "/static/media/react-redux.934eab62.PNG"
+  },
+  {
+    "revision": "7e145c9eb60da95379f070be9920eca5",
+    "url": "/static/media/twitter.7e145c9e.svg"
+  },
+  {
+    "revision": "3de182d2120823fe49c27eeb1765c53a",
+    "url": "/static/media/typescript.3de182d2.svg"
+  },
+  {
+    "revision": "c4ad090d9213612306c5283318161a40",
+    "url": "/static/media/war-react.c4ad090d.PNG"
+  }
+]);
