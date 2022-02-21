3d4ec572546ea1b3c24389ecf2197a559dc59dff
Tony <tony.w.trinh3@gmail.com>
2022-02-21 14:42:10 -0800

    Update About.js
    updated about me

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ const About = () =>{
     return (
         <div id = "about" className="about u-section-padding">
             <h2 className="about__sub-header heading-big u-margin-bottom-small ">About Me</h2>
-            <p className = "about__paragraph">I spent 4 yrs working in HVAC (Heating, Ventilation, and Air Conditioning) Industry. <br/>I am from Georgia Tech, Class of 2015. Bachelor's of Science in Mechanical Engineering. <br/>I like to play tennis in my free time and podcasting.</p>
+            <p className = "about__paragraph">I am currently working as a Front-End Developer at an e-commerce startup, developing features via Javascript and Shopify as well as working with a team of devs, product manager, and UX designer. <br/>I spent 4 yrs working in HVAC (Heating, Ventilation, and Air Conditioning) Industry. <br/>I am from Georgia Tech, Class of 2015. Bachelor's of Science in Mechanical Engineering. <br/>I like to play tennis in my free time and podcasting.</p>
         </div>
     )
 };
